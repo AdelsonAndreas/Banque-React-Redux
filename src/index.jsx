4679b7c12a41d8552259970806f221b5f4dce9f6
AdelsonAndreas <36205056+AdelsonAndreas@users.noreply.github.com>
2018-03-09 14:31:22 +0100

    oublie provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { Provider } from 'react-redux'
 
-import Acceuil from './pages/Acceuil'
-import Transactions from './pages/Transactions'
-import Nouvelle from './pages/Nouvelle'
+import Acceuil from './pages/Acceuil';
+import Transactions from './pages/Transactions';
+import Nouvelle from './pages/Nouvelle';
+import store from './store'
 
 class App extends React.Component {
     render(){
         return(
-    <Router>
-        <Switch>
-        <Route exact path="/" component={Acceuil} />
-        <Route path="/transaction" component={Transactions}></Route>
-        <Route path="/nouvelle" component={Nouvelle}></Route>
-        </Switch>
-    </Router>
+            <Provider store={store}>
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Acceuil} />
+                        <Route path="/transaction" component={Transactions}></Route>
+                        <Route path="/nouvelle" component={Nouvelle}></Route>
+                    </Switch>
+                </Router>
+            </Provider>    
         )
     }
 }
